c0762ff49e7c734cb6f8e652083c34f02ddbab29
alembicpalatable98 <tangykneel83@gmail.com>
2025-11-24 18:01:15 +0200

    Add files via upload

---
+++ ChronoCalc.py
@@ -0,0 +1,14 @@
+# calc.py - The ChronoCalc Project
+
+def subtract(a, b):
+return a - b
+
+def multiply(a, b):
+return a * b
+
+# --MAIN--
+if __name__ = "__main__":
+print("ChronoCalc v1.0")
+print(f"10*3={multiply(10,3)}")
+# TODO: Add 'add' and 'divide' functions
+
